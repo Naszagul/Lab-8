78687c278b4017bf9a9eb0bdb996b4c4e7ee7d65
regan preston <regan.preston@f8th.ai>
2022-03-31 15:42:37 -0600

    commit for lab 8 into github for submission

---
+++ build/generated-sources/ap-source-output/models/Role_.java
@@ -0,0 +1,17 @@
+package models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import models.User;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-03-31T15:35:35")
+@StaticMetamodel(Role.class)
+public class Role_ { 
+
+    public static volatile ListAttribute<Role, User> userList;
+    public static volatile SingularAttribute<Role, Integer> roleId;
+    public static volatile SingularAttribute<Role, String> roleName;
+
+}
